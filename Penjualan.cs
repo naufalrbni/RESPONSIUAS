b97dc3719828fd940da4ddc1d5a50720ff518d18
naufalrbni <naufalrbbnii@gmail.com>
2022-07-12 16:48:21 +0700

    inisialisasi project

---
+++ Penjualan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UASResponsi
+{
+    internal class Penjualan
+    {
+        public string nota { get; set; }
+        public string tanggal { get; set; }
+        public string customer { get; set; }
+        public string jenis { get; set; }
+        public double total { get; set; }
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal, SIAP PAK
+    }
+}
